28da37efabb5d47e1510540aad8bd735a08d89c2
guhmerces <guhmerces@hotmail.com>
2023-03-27 12:00:26 -0300

    Create migration file for business_qrcode table

---
+++ database/migrations/2023_03_25_214507_business_qr_code.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_qr_code', function (Blueprint $table) {
+            $table->id();
+            $table->string('owner_name');
+            $table->string('github_url');
+            $table->string('linkedin_url');
+            $table->string('qrcode_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_qr_code');
+    }
+};
