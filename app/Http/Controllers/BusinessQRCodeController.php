b6530237e6b3c4150d8f337e7917cc1e2bae49e3
guhmerces <guhmerces@hotmail.com>
2023-03-27 12:09:14 -0300

    Create store() method in BusinessQRCodeController

--- app/Http/Controllers/BusinessQRCodeController.php
+++ app/Http/Controllers/BusinessQRCodeController.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\BusinessQRCode;
+use App\Helpers\QRCodeHelper;
+use App\Http\Requests\CreateBusinessQRCodeRequest;
+use Illuminate\Support\Str;
+
 class BusinessQRCodeController extends Controller
 {
+    public function store(CreateBusinessQRCodeRequest $request, QRCodeHelper $qrCodeHelper)
+    {
+        $validatedData = $request->validated();
 
+        $dataqr = config('app.url') . "/" . $validatedData['owner_name'];
+        $fileName = Str::random() . ".png";
+
+        $qrCodeHelper->saveQRCodeFile($fileName, $dataqr);
+
+        BusinessQRCode::create(array_merge(
+            $validatedData,
+            [ 'qrcode_path' => $fileName ],
+        ));
+
+        return response('', 201);
+    }
 }
